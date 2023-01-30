f083c37809dffc66f33118f11740f3c7b3673fad
Justin <justin.delacruz@novare.com.hk>
2023-01-30 10:47:13 +0800

    Code changes

--- src/main/java/AdvanceMath.java
+++ src/main/java/AdvanceMath.java
@@ -2,18 +2,21 @@ public class AdvanceMath {
     private BasicMath basicMath;
 
     public double multiplyDifferenceBy5(double input1, double input2) {
-        double sum = basicMath.subtract(input1, input2);
-        return sum * 5;
+//        double sum = basicMath.subtract(input1, input2);
+//        return sum * 5;
+        return basicMath.subtract(input1, input2) * 5;
     }
 
     public double multiplySumBy5(double input1, double input2) {
-        double sum = basicMath.add(input1, input2);
-        return sum * 5;
+//        double sum = basicMath.add(input1, input2);
+//        return sum * 5;
+        return basicMath.multiply(basicMath.add(input1, input2), 5);
     }
 
     public double squareOfSum(double input1, double input2){
-        double sum = basicMath.add(input1, input2);
-        return sum * sum;
+//        double sum = basicMath.add(input1, input2);
+//        return sum * sum;
+        return basicMath.add(input1, input2) * basicMath.add(input1, input2);
 
     }
 
--- src/test/java/AdvancedMathTester.java
+++ src/test/java/AdvancedMathTester.java
@@ -7,6 +7,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AdvancedMathTester {
@@ -20,12 +21,14 @@ public class AdvancedMathTester {
     public void testMultiplyDifferenceBy5() {
         when(basicMath.subtract(80.0, 20.0)).thenReturn(60.0);
 
-        Assert.assertEquals(advanceMath.multiplyDifferenceBy5(80.0, 20.0), 300.0, 0);
+        Assert.assertEquals(advanceMath.multiplyDifferenceBy5(80.0, 20.0), 300, 0);
     }
 
     @Test
     public void testMultiplySumBy5() {
         when(basicMath.add(75.0, 25.0)).thenReturn(100.0);
+        when(basicMath.multiply(100.0, 5.0)).thenReturn(500.0);
+
 
         Assert.assertEquals(advanceMath.multiplySumBy5(75.0, 25.0), 500.0, 0);
     }
@@ -35,9 +38,9 @@ public class AdvancedMathTester {
         when(basicMath.add(20.0, 20.0)).thenReturn(40.0);
 
         Assert.assertEquals(advanceMath.squareOfSum(20.0, 20.0), 1600.0, 0);
-        verify(basicMath).add(20.0, 20.0);
-        verify(basicMath, atLeastOnce()).add(20.0, 20.0);
-        verify(basicMath, atMost(2)).add(20.0, 20.0);
+//        verify(basicMath).add(20.0, 20.0);
+        verify(basicMath, atLeast(2)).add(20.0, 20.0);
+        verify(basicMath, times(2)).add(20.0, 20.0);
 
     }
 
@@ -49,6 +52,52 @@ public class AdvancedMathTester {
 
         reset(basicMath);
         Assert.assertEquals(advanceMath.getPercentage(50.0, 100.0), 00.0, 0);
+    }
+
+    @Test
+    public void testMultiplyDifferenceBy5Bdd() {
+        //Given
+        given(basicMath.subtract(80.0, 20.0)).willReturn(60.0);
+        //When
+        double result = advanceMath.multiplyDifferenceBy5(80.0,20.0);
+        //Then
+        Assert.assertEquals(result, 300.0, 0);
+    }
+
+    @Test
+    public void testMultiplySumBy5Bdd() {
+        //Given
+        given(basicMath.add(75.0,25.0)).willReturn(100.0);
+        given(basicMath.multiply(100.0, 5.0)).willReturn(500.0);
+        //When
+        double result = advanceMath.multiplySumBy5(75.0,25.0);
+        //Then
+        Assert.assertEquals(result, 500.0, 0);
+
+
+    }
+
+    @Test
+    public void testSquareOfSumBdd() {
+        //Given
+        given(basicMath.add(20.0,20.0)).willReturn(40.0);
+        //When
+        advanceMath.squareOfSum(20.0,20.0);
+        //Then
+//        verify(basicMath).add(20.0, 20.0);
+        verify(basicMath, atLeast(2)).add(20.0, 20.0);
+        verify(basicMath, times(2)).add(20.0, 20.0);
+
+    }
+    @Test
+    public void testGetPercentageBdd() {
+        //Given
+        given(basicMath.add(50.0,100.0)).willReturn(0.5);
+        //When
+        double result = advanceMath.getPercentage(50.0, 100.0);
+        //Then
+        reset(basicMath);
+        Assert.assertEquals(result, 0.00, 0);
     }
 
 
